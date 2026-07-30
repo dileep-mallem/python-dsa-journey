28ffc87abd3135c5a04a1c02a200214607c6161b
dileep-mallem <mailtodileepmallem@gmail.com>
2026-07-30 18:19:44 +0530

    Revisitd , Learn Question Carefully , Nothing Over will be there

--- dsa/arrays/lc011_container_with_most_water.py
+++ dsa/arrays/lc011_container_with_most_water.py
@@ -5,21 +5,20 @@
 class Solution(object):
     def maxArea(self, height):
         l=0
-        r=len(height) - 1
-        max_area =0 
-
-        while l < r :  
+        r=len(height)-1
+        max_area=0
+        while l < r :
             width = r - l 
-            area = min([height[l],height[r]]) * width 
+            area=width * min(height[l],height[r])
 
-            if area >= max_area :
+            if area > max_area :
                 max_area=area 
-
-            if height[l] < height[r] : 
-                l+=1 
-            else  : # if greater r-=1 , if equal either one can move so itwits
+            if height[l] < height[r] :
+                l+=1
+            else :
                 r-=1
-                 
+
+
         return max_area
-# s=Solution()
-# print(s.maxArea([1,8,6,2,5,4,8,3,7]))
+s=Solution()
+print(s.maxArea([1,8,6,2,5,4,8,3,7]))
