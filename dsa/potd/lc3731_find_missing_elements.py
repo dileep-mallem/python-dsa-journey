b24caf31041e97c4901e36a4475d75e3c64ef84d
dileep-mallem <mailtodileepmallem@gmail.com>
2026-08-04 21:43:09 +0530

    Do with HashTables NExt Time

--- dsa/potd/lc3731_find_missing_elements.py
+++ dsa/potd/lc3731_find_missing_elements.py
@@ -5,11 +5,11 @@ class Solution(object):
         n=len(nums)
         # k=0
         l=[]
-        # for i in range(1,n):
-        #     while nums[k]+1 != nums[i] :
-        #         l.append(nums[k]+1)
-        #         nums[k]=nums[k]+1
-        #     k=i
-        # return l
+        for i in range(1,n):
+            while nums[k]+1 != nums[i] :
+                l.append(nums[k]+1)
+                nums[k]=nums[k]+1
+            k=i
+        return l
 
         # Using Hash Tables 
