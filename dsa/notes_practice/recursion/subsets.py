0eeacd6bd61ec501572e9f9180552f6a0a744af6
dileep-mallem <mailtodileepmallem@gmail.com>
2026-08-10 05:48:12 +0530

    Learn Handling DUolicates

--- dsa/notes_practice/recursion/subsets.py
+++ dsa/notes_practice/recursion/subsets.py
@@ -16,3 +16,33 @@ def subset1(arr) :  # Time : O(n * 2**n) Space : O(2**n *n) Total subsets , spac
 
     return outer
 print(subset1([1,2,3]))
+
+
+# Handling Duplicates 
+
+def subset2(arr : list[int] , ) : 
+    arr.sort()
+    outer = []
+    outer.append([])
+
+    start,end =0,0 
+
+
+
+    for i in range(len(arr)) : 
+        start=0 
+        # if current and prevous elemnt is same , start = end + 1
+
+        if i > 0 and arr[i]==arr[i-1] :
+            start=end+1
+        
+        n = len(outer)
+        end = n-1
+
+        for j in range(start , n) :  # **
+            internal=outer[j].copy()
+            internal.append(arr[i])
+            outer.append(internal)
+
+    return outer
+print(subset2([1,2,2]))
