89f146c9bc070d35aacac68927e2cc90e1aba59b
dileep-mallem <mailtodileepmallem@gmail.com>
2026-08-12 08:26:15 +0530

    Sliding Window ,O(n),Revisit : Yes

---
+++ dsa/potd/lc2456_max_freq_sub_array.py
@@ -0,0 +1,16 @@
+class Solution :
+    def maxSubArray(nums : list,k : int)-> int :
+        i=0
+        j=0
+        max_count=0
+        d={}
+
+        for j in range(len(nums)) : 
+            d[nums[j]]=d.get(nums[j],0)+1
+
+            while d[nums[j]]>k :
+                left=nums[i]
+                d[left]-=1
+                i+=1
+            max_count=max(max_count,j-i+1)
+        return max_count
