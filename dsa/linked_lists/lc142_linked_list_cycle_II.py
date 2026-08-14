96dd1a7870d5d34f18b0562a200549bee52e86f8
dileep-mallem <mailtodileepmallem@gmail.com>
2026-08-14 07:23:15 +0530

    Fast and Pointer Alfo , TC : O(N) , we can also return intersection point index (Wriitten in Cmnts)

---
+++ dsa/linked_lists/lc142_linked_list_cycle_II.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        # Fast and Slow pointer Algo 
+
+        fast=slow=head 
+
+        while fast and fast.next : 
+            slow=slow.next 
+            fast=fast.next.next 
+
+            if slow is fast : # Cycle Detected 
+                slow2=head 
+                # i=0
+                while slow is not slow2 : # is is faster tehn ==
+                    slow2=slow2.next
+                    slow=slow.next 
+                    # i+=1 if to return index where Cycle begins 
+
+                return slow  # return i
+        return None # If fast or fast.next become None , No cycel , so -1
